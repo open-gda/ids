032db345b0b806be9c475e6be5677a571f567fe6
Adrian Nicos <adrian.nicos@gmail.com>
2024-07-08 15:16:16 +0200

    commit to github pages

--- check_numbers.sh
+++ check_numbers.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 set -ex
-# Nazwa pliku do sprawdzenia
+
 FILE="public/list.txt"
 
-# Sprawdzenie, czy plik istnieje
+
 if [ ! -f "$FILE" ]; then
   echo "Plik $FILE nie istnieje."
   exit 1
 fi
 
-# Sprawdzenie, czy plik zawiera liczby całkowite po przecinku
-if grep -qE '([0-9]+,)*[0-9]+' "$FILE"; then
-  echo "Plik zawiera liczby całkowite po przecinku."
+if grep -qE '^[0-9]+(,[0-9]+)*$' "$FILE"; then
+  echo "Plik zawiera wyłącznie liczby całkowite oddzielone przecinkami."
 else
-  echo "Plik nie zawiera liczb całkowitych po przecinku."
+  echo "Plik nie zawiera wyłącznie liczb całkowitych oddzielonych przecinkami."
+  exit 1
 fi
